235df3d1054670704397abb0d2f4f51ca8ebe2f5
eoinlynchcodes <eoin95lynch@gmail.com>
2020-08-28 16:35:09 +0100

    That'll do pig, that'll do.

--- 4hands-react-app/src/components/CurrentMenu.js
+++ 4hands-react-app/src/components/CurrentMenu.js
@@ -3,22 +3,23 @@ import axios from 'axios';
 
 function CurrentMenu(){
 
-    // const [menu, setMenu] = useState();
+    const [menu, setMenu] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3300/')
+        axios.get('http://localhost:3300/menu')
         .then(res => {
-            console.log(res);
+            console.log(res.data);
+            setMenu(res.data);
         })
         .catch(error => {
             console.log(error);
         })
-    })
+    }, []);
 
     return (
         <div>
             <h2>The menu being displayed currently is:</h2>
-
+         
         </div>
     );
 }
